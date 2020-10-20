a126b93b537ee8e19c7478168c10e2b6b1700d43
Kenneth Schiller <Kenadia@users.noreply.github.com>
2020-10-20 15:57:46 -0700

    Add axios module (#3)
    * Add axios module
    
    * Minor fixes
    
    * Fix lint

---
+++ src/lib/axios/axiosRequest.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+import {
+  AxiosServerError,
+  AxiosError,
+} from './errors';
+
+export async function axiosRequest(options: AxiosRequestConfig): Promise<{}> {
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error) {
+    if (error.isAxiosError) {
+      if (error.response) {
+        throw new AxiosServerError(error.response, error);
+      }
+      throw new AxiosError(`Axios: ${error.message}`, error);
+    }
+    throw error;
+  }
+}
