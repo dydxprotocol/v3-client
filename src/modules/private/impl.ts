ae44ff80b917b804a864777e3bbeeadc038c7602
samweinberg23 <samweinberg23@gmail.com>
2020-10-22 16:19:48 -0700

    Add remaining private endpoints except withdrawal (#6)
    * cleanup existing endpoints
    
    * fix
    
    * additional endpoints
    
    * ks1

--- src/modules/private/impl.ts
+++ src/modules/private/impl.ts
@@ -6,6 +6,9 @@ import {
   asSimpleKeyPair,
   ApiMethod,
   ApiRequest,
+  OrderSide,
+  OrderType,
+  Asset,
 } from '@dydxprotocol/starkex-lib';
 
 import {
@@ -14,9 +17,13 @@ import {
 } from '../../lib/axios';
 import { getAccountId } from '../../lib/db';
 import {
+  AccountAction,
   ApiOrder,
   ISO8601,
+  Market,
+  OrderStatus,
   PartialBy,
+  PositionStatus,
 } from '../../types';
 
 export default class Private {
@@ -36,11 +43,14 @@ export default class Private {
     }
   }
 
+  // ============ Request Helpers ============
+
   protected async post(
     endpoint: string,
     data: {},
   ): Promise<{}> {
     const url: string = `/v3/${endpoint}`;
+    const expiresAt: ISO8601 = new Date().toString();
     return axiosRequest({
       url: `${this.host}${url}`,
       method: RequestMethod.POST,
@@ -49,11 +59,11 @@ export default class Private {
         'DYDX-SIGNATURE': this.generateSignature({
           requestPath: url,
           method: ApiMethod.POST,
-          expiresAt: new Date().toISOString(),
+          expiresAt,
           body: data,
         }),
         'DYDX-API-KEY': this.apiKeyPair.publicKey,
-        'DYDX-TIMESTAMP': new Date().toISOString(),
+        'DYDX-TIMESTAMP': expiresAt,
       },
     });
   }
@@ -62,6 +72,7 @@ export default class Private {
     endpoint: string,
   ): Promise<{}> {
     const url: string = `/v3/${endpoint}`;
+    const expiresAt: ISO8601 = new Date().toString();
     return axiosRequest({
       url: `${this.host}${url}`,
       method: RequestMethod.GET,
@@ -69,13 +80,35 @@ export default class Private {
         'DYDX-SIGNATURE': this.generateSignature({
           requestPath: url,
           method: ApiMethod.GET,
-          expiresAt: new Date().toISOString(),
+          expiresAt,
         }),
         'DYDX-API-KEY': this.apiKeyPair.publicKey,
-        'DYDX-TIMESTAMP': new Date().toISOString(),
+        'DYDX-TIMESTAMP': expiresAt,
       },
     });
   }
+
+  protected async delete(
+    endpoint: string,
+  ): Promise<{}> {
+    const url: string = `/v3/${endpoint}`;
+    const expiresAt: ISO8601 = new Date().toString();
+    return axiosRequest({
+      url: `${this.host}${url}`,
+      method: RequestMethod.DELETE,
+      headers: {
+        'DYDX-SIGNATURE': this.generateSignature({
+          requestPath: url,
+          method: ApiMethod.DELETE,
+          expiresAt,
+        }),
+        'DYDX-API-KEY': this.apiKeyPair.publicKey,
+        'DYDX-TIMESTAMP': expiresAt,
+      },
+    });
+  }
+
+  // ============ Requests ============
 
   async getUser(): Promise<{}> {
     return this.get(
@@ -114,9 +147,45 @@ export default class Private {
     );
   }
 
-  async getPositions(): Promise<void> {}
-  async getOrders(): Promise<void> {}
-  async getOrder(): Promise<void> {}
+  async getPositions(
+    params: {
+      market?: Market,
+      status?: PositionStatus,
+      limit?: number,
+      createdBeforeOrAt?: ISO8601,
+    },
+  ): Promise<{}> {
+    return this.get(
+      this.generateQueryPath('positions', params),
+    );
+  }
+
+  async getOrders(
+    params: {
+      market?: Market,
+      status?: OrderStatus,
+      side?: OrderSide,
+      type?: OrderType,
+      limit?: number,
+      createdBeforeOrAt?: ISO8601,
+    },
+  ): Promise<{}> {
+    return this.get(
+      this.generateQueryPath('orders', params),
+    );
+  }
+
+  async getOrderById(orderId: string): Promise<{}> {
+    return this.get(
+      `orders/${orderId}`,
+    );
+  }
+
+  async getOrderByClientId(clientId: string): Promise<{}> {
+    return this.get(
+      `orders/client/${clientId}`,
+    );
+  }
 
   async createOrder(
     params: PartialBy<ApiOrder, 'clientId' | 'signature'>,
@@ -156,11 +225,42 @@ export default class Private {
     );
   }
 
-  async deleteOrder(): Promise<void> {}
-  async cancelOrder(): Promise<void> {}
-  async cancelAllOrders(): Promise<void> {}
-  async getFills(): Promise<void> {}
-  async getTransfers(): Promise<void> {}
+  async deleteOrder(orderId: string): Promise<{}> {
+    return this.delete(
+      `orders/${orderId}`,
+    );
+  }
+
+  async deleteAllOrders(params: { market?: Market }): Promise<{}> {
+    return this.delete(
+      this.generateQueryPath('orders', params),
+    );
+  }
+
+  async getFills(
+    params: {
+      market?: Market,
+      orderId?: string,
+      limit?: number,
+      createdBeforeOrAt?: ISO8601,
+    },
+  ): Promise<{}> {
+    return this.get(
+      this.generateQueryPath('fills', params),
+    );
+  }
+
+  async getTransfers(
+    params: {
+      type?: AccountAction,
+      limit?: number,
+      createdBeforeOrAt?: ISO8601,
+    },
+  ): Promise<{}> {
+    return this.get(
+      this.generateQueryPath('transfers', params),
+    );
+  }
 
   // TODO: Fix. See createOrder above.
   // async createWithdrawal(
@@ -190,9 +290,32 @@ export default class Private {
   //   });
   // }
 
-  async createDeposit(): Promise<void> {}
+  async createDeposit(
+    params: {
+      amount: string,
+      asset: Asset,
+      fromAddress: string,
+    },
+  ): Promise<{}> {
+    return this.post(
+      'deposits',
+      params,
+    );
+  }
 
-  async getFundingPayments(): Promise<void> {}
+  async getFundingPayments(
+    params: {
+      market?: Market,
+      limit?: number,
+      effectiveBeforeOrAt?: ISO8601,
+    },
+  ): Promise<{}> {
+    return this.get(
+      this.generateQueryPath('funding', params),
+    );
+  }
+
+  // ============ Request Generation Helpers ============
 
   private generateSignature({
     requestPath,
@@ -212,5 +335,17 @@ export default class Private {
       publicKey: this.apiKeyPair.publicKey,
       expiresAt,
     }).sign(this.apiKeyPair.privateKey);
+  }
+
+  private generateQueryPath(url: string, params: {}): string {
+    const entries = Object.entries(params);
+    if (!entries.length) {
+      return url;
+    }
+
+    const paramsString = entries.map(
+      (kv) => `${kv[0]}=${kv[1]}`,
+    ).join('&');
+    return `${url}?${paramsString}`;
   }
 }
