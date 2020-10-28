947a438173f84df05e41e30e4c73e3dcfa07a73f
Kenneth Schiller <Kenadia@users.noreply.github.com>
2020-10-28 10:42:46 -0700

    Add test for signing

---
+++ __tests__/keys.test.ts
@@ -0,0 +1,32 @@
+/**
+ * Unit tests for the API keys module.
+ */
+
+import axios, { AxiosResponse } from 'axios';
+import Web3 from 'web3';
+
+import { DydxClient, EthereumAccount } from '../src';
+import { asMock } from './util';
+
+describe('API Keys Module', () => {
+
+  it('Signs a request', async () => {
+    asMock(axios).mockResolvedValue({} as AxiosResponse);
+
+    const web3 = new Web3();
+    const account: EthereumAccount = web3.eth.accounts.wallet.create(1)[0];
+    const client = new DydxClient('https://example.com', { web3 });
+    await client.keys.getApiKeys(account.address);
+
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      url: expect.stringContaining('/v3/api-keys'),
+      method: 'GET',
+      headers: {
+        'DYDX-SIGNATURE': expect.stringMatching(/0x[0-9a-f]{130}/),
+        'DYDX-TIMESTAMP': expect.any(String),
+        'DYDX-ETHEREUM-ADDRESS': expect.stringMatching(/0x[0-9a-fA-F]{40}/),
+      }
+    });
+  });
+});
