0a916a3bdeb069e24dde1775752397d61bed5175
Sam Weinberg <samweinberg23@gmail.com>
2020-10-29 11:45:40 -0700

    lint

--- src/modules/onboarding/impl.ts
+++ src/modules/onboarding/impl.ts
@@ -27,7 +27,7 @@ export default class Onboarding {
     // TODO: Get ethereumAddress from the provider (same address used for signing).
     ethereumAddress: string,
   ): Promise<Data> {
-    const signature: string = await this.signRequest(ethereumAddress);
+    // const signature: string = await this.signRequest(ethereumAddress);
 
     const url: string = `/v3/${endpoint}`;
     return axiosRequest({
