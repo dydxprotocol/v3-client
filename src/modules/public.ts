440c4d79250e52e9271b18ea08c0b896cabf8a36
Sam Weinberg <samweinberg23@gmail.com>
2020-12-01 11:12:03 -0800

    add documentation

--- src/modules/public.ts
+++ src/modules/public.ts
@@ -19,6 +19,11 @@ export default class Public {
     this.host = host;
   }
 
+  /**
+   * @description check if a user exists for an ethereum address
+   *
+   * @param ethereumAddress of the user
+   */
   checkIfUserExists(
     ethereumAddress: string,
   ): Promise<{ exists: boolean }> {
@@ -26,6 +31,11 @@ export default class Public {
     return this.sendPublicGetRequest(uri, { ethereumAddress });
   }
 
+  /**
+   * @description check if a username already exists
+   *
+   * @param username being queried
+   */
   checkIfUsernameExists(
     username: string,
   ): Promise<{ exists: boolean }> {
@@ -33,11 +43,21 @@ export default class Public {
     return this.sendPublicGetRequest(uri, { username });
   }
 
+  /**
+   * @description get market information for either all markets or a specific market
+   *
+   * @param market if only one market should be returned
+   */
   getMarkets(market?: Market): Promise<{ markets: MarketResponseObject }> {
     const uri: string = 'v3/markets';
     return this.sendPublicGetRequest(uri, { market });
   }
 
+  /**
+   * @description get orderbook for a specific market
+   *
+   * @param market being queried
+   */
   getOrderBook(market: Market): Promise<{
     orderbook: {
       bids: OrderbookResponseOrder[],
@@ -47,6 +67,14 @@ export default class Public {
     return this.sendPublicGetRequest(`v3/orderbook/${market}`, {});
   }
 
+  /**
+   * @description get one or more market specific statistics for a time period
+   *
+   * @param {
+   * @market being queried
+   * @days if a specific time period statistic should be returned
+   * }
+   */
   getStats({
     market,
     days,
@@ -59,6 +87,12 @@ export default class Public {
     return this.sendPublicGetRequest(uri, { days });
   }
 
+  /**
+   * @description get trades for a market up to a certain time
+   *
+   * @param market being checked
+   * @param startingBeforeOrAt latest trade being returned
+   */
   getTrades({
     market,
     startingBeforeOrAt,
@@ -72,6 +106,12 @@ export default class Public {
 
   }
 
+  /**
+   * @description get historical funding rates for a market up to a certain time
+   *
+   * @param market being checked
+   * @param effectiveBeforeOrAt latest historical funding rate being returned
+   */
   getHistoricalFunding(market: Market, effectiveBeforeOrAt: ISO8601):
   Promise<{ historicalFunding: HistoricalFundingResponseObject }> {
     const uri: string = `v3/historical-funding/${market}`;
