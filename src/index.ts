b626054cd91d35bf6ec65d0e24fc859dd473b0ee
Sam Weinberg <samweinberg23@gmail.com>
2020-12-14 10:31:52 -0800

    export axios request for internal use

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as DydxClient } from './dydx-client';
 export * from './lib/eth-validation/actions';
 export * from './modules/sign-off-chain-action';
 export * from './types';
+export * from './lib/axios/axiosRequest';
