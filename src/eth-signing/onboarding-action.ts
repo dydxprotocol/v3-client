353e9ea16e87f48725c7fec13ea5e7b991edf4b5
Kenadia <Kenadia@users.noreply.github.com>
2021-02-02 18:33:23 -0800

    v0.7.3 Add method for API key recovery (#58)
    * Add method for API key recovery
    
    * v0.7.3

--- src/eth-signing/onboarding-action.ts
+++ src/eth-signing/onboarding-action.ts
@@ -4,8 +4,8 @@
  * These are used during onboarding. The signature must be deterministic based on the Ethereum key
  * because the signatures will be used for key derivation, and the keys should be recoverable:
  *   - The onboarding signature is used to derive the default API credentials, on the server.
- *   - The key derivation signature is used by the UI to derive the STARK key pair. Programmatic
- *     traders may optionally derive their STARK key pair in the same way.
+ *   - The key derivation signature is used by the frontend app to derive the STARK key pair.
+ *     Programmatic traders may optionally derive their STARK key pair in the same way.
  */
 
 import Web3 from 'web3';
--- src/helpers/request-helpers.ts
+++ src/helpers/request-helpers.ts
@@ -1,3 +1,7 @@
+import Web3 from 'web3';
+
+import { stripHexPrefix } from '../eth-signing/helpers';
+
 export function generateQueryPath(url: string, params: {}): string {
   const definedEntries = Object.entries(params)
     .filter(([_key, value]: [string, unknown]) => value !== undefined);
@@ -10,4 +14,11 @@ export function generateQueryPath(url: string, params: {}): string {
     ([key, value]: [string, unknown]) => `${key}=${value}`,
   ).join('&');
   return `${url}?${paramsString}`;
+}
+
+export function keccak256Buffer(input: Buffer): Buffer {
+  if (input.length === 0) {
+    throw new Error('keccak256Buffer: Expected a Buffer with non-zero length');
+  }
+  return Buffer.from(stripHexPrefix(Web3.utils.soliditySha3(input as unknown as string)!), 'hex');
 }
