1c6a3f92ba487c212f52cd51dcde5176ee9051d1
Kenneth Schiller <Kenadia@users.noreply.github.com>
2021-03-04 12:16:04 -0800

    Fix lint

--- __tests__/eth-signing/onboarding-action.test.ts
+++ __tests__/eth-signing/onboarding-action.test.ts
@@ -15,7 +15,6 @@ let remoteAccountAddress: string;
 const GANACHE_ADDRESS = '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1';
 const GANACHE_PRIVATE_KEY = '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d';
 
-
 // IMPORTANT: This is the message used with the SigningMethod.PERSONAL singing method.
 //            The message should not be changed at all since it's used to generated default keys.
 const EXPECTED_PERSONAL_MESSAGE = `{
