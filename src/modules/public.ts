65c94fadd3dd8fd7721eed17b928e7f1e19b0d37
samweinberg23 <samweinberg23@gmail.com>
2021-09-03 16:30:23 -0700

    v1.3.0: make stats market optional (#122)
    * make stats market optional
    
    * fix

--- src/modules/public.ts
+++ src/modules/public.ts
@@ -108,10 +108,12 @@ export default class Public {
     market,
     days,
   }: {
-    market: Market,
+    market?: Market,
     days?: MarketStatisticDay,
   }): Promise<{ markets: MarketStatisticResponseObject }> {
-    const uri: string = `stats/${market}`;
+    const uri: string = market !== undefined
+      ? `stats/${market}`
+      : 'stats';
     return this.get(uri, { days });
   }
 
